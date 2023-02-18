d59ddaba03e19d77d2acc2c1c2d1f33d971bc1e3
chinyere92 <chinyereerondu@gmail.com>
2023-02-17 18:07:22 -0800

    C shege

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,28 +1,24 @@
 #include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
-/* more headers goes there */
-
-/* betty style doc for function main goes tihere */
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
  */
 int main(void)
 {
-	int n;
+	int c = 0;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 0)
-		printf("%d is positive\n", n);
-	if (n == 0)
-		printf("%d is zero\n", n);
-	if (n < 0)
-		printf("%d is negative\n", n);
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
 	return (0);
 }
