d170143098f216ccebdf071dd765ea8a9e6b3f19
chinyereerondu <chinyereerondu21@gmail.com>
2023-12-18 14:29:47 -0800

    num wit puchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * print number in base 10 using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar('0' +i);
+	putchar('\n');
+
+	return (0);
+}
