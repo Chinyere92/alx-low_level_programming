7a2efe498a2f4533649662301e2534c394700b65
chinyereerondu <chinyereerondu21@gmail.com>
2023-12-20 14:03:45 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+/**
+ * print_sign - print sigh
+ * @n: num to evaluate
+ * Return: 0 / 1 / -1
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n > 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+		_putchar(0);
+	return (0);
+}
