6401902af921f9392d9f0a5ecd872ba3c9960983
chinyereerondu <chinyereerondu21@gmail.com>
2023-12-20 13:08:24 -0800

    is lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+/**
+ *  _islower - checking for lower leter
+ *  @c: letter to check
+ *  Return: true / false
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c >= 'z')
+		return (1);
+	return (0);
+}
