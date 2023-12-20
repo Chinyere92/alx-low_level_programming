d8f80bd75140c2aadbdd9e8d286bad31d9234b18
chinyereerondu <chinyereerondu21@gmail.com>
2023-12-20 01:56:24 -0800

    x10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+/**
+ * print_alphabet_x10 - print alphabet x10
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	char y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+			_putchar(y);
+		_putchar('\n');
+	}
+}
