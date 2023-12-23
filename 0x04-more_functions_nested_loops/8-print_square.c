97bf1a3b633a596a627a737863f0d7f6036e0068
chinyereerondu <chinyereerondu21@gmail.com>
2023-12-22 20:33:42 -0800

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - square
+ * @size: size of square
+ * Return: void function
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
