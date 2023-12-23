f01da31f3da6052b5cac96c9766fe0f57c6bbdf1
chinyereerondu <chinyereerondu21@gmail.com>
2023-12-22 20:17:47 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.c"
+/**
+ * print_diagonal - print diagonal
+ * @n: amount of times to print
+ * Return: void function
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+	{
+		_Putchar('\n');
+		return;
+	}
+
+	for (x = 0; x < n; x++)
+	{
+		for (y = 0; y <= x; y++)
+		{
+			if (y == x)
+				_putchar('\\');
+			else
+				_putchar(' ');
+		}
+
+	}
+}
