4cd5264236cdcabbbb537b7e27755499d0015df2
chinyereerondu <chinyereerondu21@gmail.com>
2023-12-22 20:02:11 -0800

    print _

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - print line
+ * @n: nth time to print line
+ * Return: void funct
+ */
+void print_line(int n)
+{
+	int m;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (m = 0; m < n; m++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
