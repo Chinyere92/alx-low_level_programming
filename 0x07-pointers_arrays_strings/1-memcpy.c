b59edfebdf43bc5e9e2c087e5fbbac470cc8cba6
chinyereerondu <chinyereerondu21@gmail.com>
2024-01-08 19:20:12 -0800

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+/**
+ * _memcpy - copy memory
+ * @dest: destination to copy to
+ * @src: srcour where to copy from
+ * @n: nth
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *xy = dest;
+
+	while (n--)
+		*xy++ = *src++;
+	return (dest);
+}
