89e249de70d6d1daa77a87a44bcbcd4edeff8260
chinyereerondu <chinyereerondu21@gmail.com>
2024-01-08 19:32:42 -0800

    pbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+/**
+ * _strpbrk - locates the first occurrence in the string
+ * @s: string
+ * @accept: word to search
+ * Return: s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+
+	while (*s != '\0')
+	{
+		a = accept;
+		while (*a != '\0')
+		{
+			if (*s == *a)
+				return (s);
+			a++;
+		}
+		s++;
+	}
+	return ('\0');
+}
