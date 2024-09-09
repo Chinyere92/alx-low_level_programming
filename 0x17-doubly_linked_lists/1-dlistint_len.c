4aa43b0a1981449c1f58c795c480757cf90d2768
danimohli <danimohlife@gmail.com>
2024-09-09 15:50:12 +0100

    node len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - count total nodes in dlistint
+ * @h: head of double linked list
+ * Return: Total number of Node
+ */
+size_t size_t dlistint_len(const dlistint_t *h)
+{
+	int node_total = 0;
+
+	while (h != NULL)
+	{
+		node_total += 1;
+		h = h->next;
+	}
+	return (node_total);
+}
